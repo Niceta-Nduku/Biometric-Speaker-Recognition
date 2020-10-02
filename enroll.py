aead42f6be6b0df2ffac06ae60551a0b56d377ef
Niceta-Nduku <40625399+Niceta-Nduku@users.noreply.github.com>
2020-10-02 22:10:38 +0200

    added UBM training example

--- enroll.py
+++ enroll.py
@@ -1,6 +1,11 @@
-import threading
-import time
 import getpass
+import os
+import argparse
+import numpy as np
+from scipy.io import wavfile
+import python.features as mfcc
+import sklearn.mixture as skm
+from hmmlearn import hmm
 
 class enroll():
 
@@ -18,7 +23,42 @@ class enroll():
     def record(self):
         #release promp
         pass
+
+    def getFeatures(self,speaker_folder):
+
+        speaker_features = {}
+
+        # if not os.path.isdir(speaker_folder):
+        #     continue #print error
+
+        for digit in os.listdir(speaker_folder):
+
+            digit_folder =  os.path.join(speaker_folder,digit)
+            
+            digit_features = []
+
+            features = np.asarray(())
+
+            for filename in [x for x in os.listdir(digit_folder) if x.endswith('.wav')][:5]:
+
+                filepath = os.path.join(digit_folder,filename)
+                sampling_rate, audio_signal = wavfile.read(filepath)
+
+                feat = mfcc.getMFCC(audio_signal,sampling_rate)
+
+                if features.size == 0:
+                    features = feat
+                else:
+                    features = np.vstack((features, feat))
+
+            digit_features.append(features)
+            speaker_features[digit] = digit_features
+
+
     def adapt(self):
+        #get digit ubm
+        #predict
+        
         pass
     def save(self):
         pass
---
+++ python/Models.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+import numpy as np
+from scipy.io import wavfile
+import python.features as mfcc
+import sklearn.mixture as skm
+from hmmlearn import hmm
+
+class UBM(object):
+
+    def __init__(self,num_coef):
+
+        self.digit_features = {}
+        self.all_digit_UBM = {}
+
+    def loadtrainingfeatures(self,input_folder):
+
+        self.digit_features = {}
+
+        for dirname in os.listdir(input_folder):
+
+            digit_folder = os.path.join(input_folder,dirname)
+
+            if not os.path.isdir(digit_folder):
+                continue
+
+            all_speaker_features  = []
+
+            for speaker in os.listdir(digit_folder):
+
+                speaker_folder =  os.path.join(digit_folder,speaker)
+
+
+                features = np.asarray(())
+
+                for filename in [x for x in os.listdir(speaker_folder) if x.endswith('.wav')][:5]:
+
+                    filepath = os.path.join(speaker_folder,filename)
+                    sampling_rate, audio_signal = wavfile.read(filepath)
+
+                    feat = mfcc.getMFCC(audio_signal,sampling_rate)
+
+                    if features.size == 0:
+                        features = feat
+                    else:
+                        features = np.vstack((features, feat))
+
+                all_speaker_features.append(features)
+
+            self.digit_features[dirname] = all_speaker_features
+        
+    
+
+    def trainUBM(self):
+
+        for digit in self.digit_features:
+            digit_hmm = hmm.GMMHMM(n_components=8, n_iter=100, init_params="smt", covariance_type='diag',algorithm="viterbi")
+            digit_gmms = []
+            for speaker in self.digit_features[digit]:
+                speaker_gmm = skm.GaussianMixture(n_components=8,covariance_type='diag')
+                speaker_gmm.fit(speaker)
+                digit_gmms.append(speaker_gmm)
+            
+            digit_hmm.gmm_ = digit_gmms
+            self.all_digit_UBM[digit] = digit_hmm
+
+    def saveUBM(self):
+        pass
+
+    def loadUBM(self):
+        pass    
+
+
+
+
+
+
+                
+if __name__ == "__main__":
+    pass
